2960ed3046fc57ad605d1f82e55e2db2dc5c45ea
Keith <keith.ong5000@gmail.com>
2020-03-12 16:49:28 -0700

    Add ProjectCard component to hold information about various coding projects

--- src/App.js
+++ src/App.js
@@ -4,12 +4,49 @@ import './App.css';
 import Container from 'react-bootstrap/Container'
 import NavigationBar from './components/NavigationBar'
 import HomepageHero from './components/HomepageHero'
+import ProjectCard from './components/ProjectCard'
+import gameImage from './assets/game.png'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 function App() {
   return (
     <Container fluid className="head-container">
-      <NavigationBar/>
-      <HomepageHero/>
+      <NavigationBar />
+      <HomepageHero />
+      <h2 id="projects-header">PROJECTS</h2>
+      <Row className="card-holder">
+        <Col className="col-lg-4">
+          <ProjectCard
+            image={gameImage}
+            title="Android Game"
+            text="A retro sidescroller mobile game"
+            skills={[
+              "Java", "Android", "Game Design"
+            ]}
+          />
+        </Col>
+        <Col className="col-lg-4">
+          <ProjectCard
+            image={gameImage}
+            title="Android Game"
+            text="A retro sidescroller mobile game"
+            skills={[
+              "Java", "Android", "Game Design"
+            ]}
+          />
+        </Col>
+        <Col className="col-lg-4">
+          <ProjectCard
+            image={gameImage}
+            title="Android Game"
+            text="A retro sidescroller mobile game"
+            skills={[
+              "Java", "Android", "Game Design"
+            ]}
+          />
+        </Col>
+      </Row>
     </Container>
   );
 }
---
+++ src/components/ProjectCard.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+
+
+
+const ProjectCard = ( props ) => (
+    <Card style={{ width: '18rem' }} className="project-card">
+        <Card.Img variant="top" src={props.image} className="card-image"/>
+        <Card.Body>
+            <Card.Title>{props.title}</Card.Title>
+            <Card.Text>{props.text}</Card.Text>
+            <Button variant="primary">Github</Button>
+            <Button variant="primary">Site</Button>
+        </Card.Body>
+    </Card>
+)
+export default ProjectCard
